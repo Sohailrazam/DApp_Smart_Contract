e7a2f4c003fa80676b95e5954945ae790f634d1f
Sohail Raza <sohailmalik059@gmail.com>
2022-02-11 19:03:32 +0500

    testing-contract

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
+import { InjectedConnector } from '@web3-react/injected-connector'
+import React, { useEffect } from 'react';
 import './App.css';
+import Web3 from 'web3';
+import greeting from './abi/greeting.json'
+import { AbiItem } from 'web3-utils';
+import { useWeb3React } from '@web3-react/core'
+
+
+declare let window: any;
 
 function App() {
+  const web3 = new Web3(Web3.givenProvider || 'https://rinkeby.infura.io/v3/26df008675844a13a371c616a1a7328d');
+  const greeting_contract = new web3.eth.Contract((greeting as unknown) as AbiItem, '0x943D3F370354046DB255e94333B9b8f3bc794deD');
+  const { active, account, library, connector, activate, deactivate } = useWeb3React()
+
+  async function connect() {
+    try {
+      await activate(injected)
+    } catch (ex) {
+      console.log('error on connecting', ex)
+    }
+  }
+
+  async function disconnect() {
+    try {
+      deactivate()
+    } catch (ex) {
+      console.log('disconnect error: ', ex)
+    }
+  }
+
+  const injected = new InjectedConnector({
+    supportedChainIds: [1, 3, 4, 5, 42],
+  })
+  const changeGreeting = async () => {
+    try {
+      return await greeting_contract.methods.changeGreeting('Hello_Sohail')
+    } catch (error) {
+      console.error(error);
+    }
+    console.log('called');
+  };
+
+  useEffect(() => {
+    console.log('web3', greeting_contract.methods);
+    web3.eth.getAccounts().then(console.log);
+    (async () => {
+      const myre = await greeting_contract.methods.greet().call();
+      console.log('myre: ', myre);
+    })()
+
+    if (window.ethereum.isMetaMask) {
+      console.log('metas');
+    }
+    else {
+      console.log('metamask not installed');
+
+    }
+  }, [active, account])
+
+  function BlanacedBracket(str: string) {
+    const openers = ['(', '{', '['];
+    const closers = [')', '}', ']'];
+    var stack: any = [];
+    var dict: any = {
+      '{': '}',
+      '(': ')',
+      '[': ']'
+    }
+
+    for (let i = 0; i < str.length; i++) {
+      let char = str[i];
+      if (openers.includes(char)) {
+        stack.push(char)
+      }
+      else if (closers.includes(char)) {
+        if (!stack.length) {
+          return false;
+        }
+
+        else if (dict[stack.pop()] !== char) {
+            return false;
+        }
+      }
+    }
+    console.log('stack: ',stack);
+    
+    return stack.length === 0
+  }
+
+  console.log(BlanacedBracket('sohai)l('));
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <div className="flex flex-col items-center justify-center">
+        <button onClick={connect} className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">Connect to MetaMask</button>
+        {active ? <span>Connected with <b>{account}</b></span> : <span>Not connected</span>}
+        <button onClick={disconnect} className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">Disconnect</button>
+      </div>
+      <button onClick={() => changeGreeting()}>change greeting</button> */}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Web3ReactProvider } from '@web3-react/core'
 import reportWebVitals from './reportWebVitals';
+import Web3 from 'web3';
+
+export const getLibrary = (provider: any): Web3 => {
+  return provider
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <App />
+    </Web3ReactProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
